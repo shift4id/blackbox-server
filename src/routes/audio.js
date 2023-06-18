c02b2e460f0dd584a23b23b9715e3791146dd3ca
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2023-06-18 09:05:13 -0700

    Users Backend

--- src/routes/audio.js
+++ src/routes/audio.js
@@ -1,9 +1,9 @@
-const audio = require("../controllers/audio");
+// const audio = require("../controllers/audio");
 
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-router.post("/", audio.submitAudio);
-router.get("/", audio.getPredictions);
+// router.post("/", audio.submitAudio);
+// router.get("/", audio.getPredictions);
 
-module.exports = router;
+// module.exports = router;
